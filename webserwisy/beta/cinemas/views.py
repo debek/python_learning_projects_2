fad6e067d1342ad2fc07184ef9380198660a1bad
Daniel Debny <debekgithub@gmail.com>
2022-04-19 22:50:41 +0200

    Fix projections in idex.hrml

--- webserwisy/beta/cinemas/views.py
+++ webserwisy/beta/cinemas/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie, Projection
 
+# Rozwiązanie 1, powiazane z index.html
+# highlights = Movie.objects.prefetch_related('projection_set').all()
 
-highlights = Movie.objects.prefetch_related('projection_set').all()[:]
+# Rozwiązanie 2, powiazane z index.html
+highlights = Projection.objects.prefetch_related().all()
 
 def home(request):
     context = {
