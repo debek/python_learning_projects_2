2549b0f9846015728d17951a484c740d0c1d8309
Daniel Debny <debekgithub@gmail.com>
2022-05-21 12:30:59 +0200

    Commit just for safe

--- webserwisy/gamma/buildingmanagement/models.py
+++ webserwisy/gamma/buildingmanagement/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+#Jak cos sie wywali todczas procesu to przywraca do bazy jakby nic sie nie stalo .transactions
+from django.db import transaction
+from django.db.models import F
 
 class Room(models.Model):
     name = models.CharField(max_length=100)
@@ -7,7 +10,12 @@ class Room(models.Model):
     def __str__(self):
         return self.name
 
+    @transaction.atomic
     def move_people_t(self, other_room, count=1):
+
+        #Msowo mozemy podmienic rzeczy w QuerySet
+        Room.objects.filter(pk=self.pk).update(people_count=F("people_count") - count)
+
         self.people_count -= count
         self.save()
         other_room.people_count += count
